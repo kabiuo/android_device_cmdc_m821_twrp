6c7245ad0782f8b40ee8cb90a40b46b0c07428f1
kabiuo <852119338@qq.com>
2020-03-16 16:36:36 +0800

    upload cmdc m821 twrp device files

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# android_device_cmdc_m821_twrp
+# android_device_twrp_tree_lettuce
+
+#Twrp_Device_Tree_For_Lettuce
+
---
+++ omni_m821.mk
@@ -0,0 +1,44 @@
+#
+# Copyright 2012 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+LOCAL_PATH := device/cmdc/m821
+
+# Charger
+PRODUCT_PACKAGES += charger charger_res_images
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/dt.img:dt.img \
+    $(LOCAL_PATH)/kernel:kernel \
+    $(LOCAL_PATH)/fstab.qcom:root/fstab.qcom \
+    $(LOCAL_PATH)/recovery.fstab:root/etc/recovery.fstab
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := m821
+PRODUCT_NAME := omni_m821
+PRODUCT_BRAND := CMDC
+PRODUCT_MODEL := m821
+PRODUCT_MANUFACTURER := cmdc
+#Wingtech
+
+# enable stock zip packages flash
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.build.product=m821 \
+    ro.product.device=m821
